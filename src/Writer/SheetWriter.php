9715e81d2c250cbfa8206cc361c743e2bc5e5ae7
mosharaf <mosharaf@pixmama.com>
2021-04-28 10:24:49 +0000

    Add spout based sheet writer

---
+++ src/Writer/SheetWriter.php
@@ -0,0 +1,95 @@
+<?php
+
+
+namespace Sigmasolutions\Sheets\Writer;
+
+
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Sigmasolutions\Sheets\Exceptions\SigmaSheetException;
+
+class SheetWriter
+{
+    /**
+     * @var WriterEntityFactory
+     */
+    private $writer;
+    /**
+     * @var string
+     */
+    private $writerType;
+
+    /**
+     * SheetWriter constructor.
+     * @param $writerType
+     * @throws \Box\Spout\Common\Exception\UnsupportedTypeException
+     */
+    public function __construct($writerType)
+    {
+        $this->writerType = $writerType;
+        $this->writer = WriterEntityFactory::createWriter($writerType);
+    }
+
+    /**
+     * @param $filePath
+     * @param string $writerType
+     * @return SheetWriter
+     * @throws \Box\Spout\Common\Exception\IOException
+     * @throws \Box\Spout\Common\Exception\UnsupportedTypeException
+     */
+    public static function open($writerType = 'xlsx')
+    {
+        try {
+            return (new SheetWriter($writerType));
+        }catch (\Exception $exception){
+            throw new SigmaSheetException($exception->getMessage());
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getWriterType()
+    {
+        return $this->writerType;
+    }
+
+    /**
+     * @param string $filePath
+     * @param array $rows
+     * @throws SigmaSheetException
+     */
+    public function write(string $filePath, array $rows): void
+    {
+        try {
+            $this->writer->openToFile($filePath);
+            if (is_string($rows[0])) {
+                $this->writer->addRow(WriterEntityFactory::createRowFromArray($rows));
+            }
+            $this->writer->addRows($this->getSpoutEquivalentRows($rows));
+        } catch (\Exception $exception) {
+            throw new SigmaSheetException($exception->getMessage());
+        } finally {
+            $this->close();
+        }
+    }
+
+    /**
+     * @param array $rows
+     * @return array|\Box\Spout\Common\Entity\Row[]
+     */
+    private function getSpoutEquivalentRows(array $rows)
+    {
+        return array_map(function (array $row) {
+            return WriterEntityFactory::createRowFromArray($row);
+        }, $rows);
+    }
+
+    /**
+     * @return void
+     */
+    public function close()
+    {
+        $this->writer->close();
+    }
+
+}
